0cb7fdc6b9ba7067b85c94c11a1bf10ef626cfa4
Allen Hu <allenhu1029@gmail.com>
2020-04-15 13:12:55 +0800

    Added an install script

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-username=$(who am i | awk '{print $1}')
+readonly username=$(who am i | awk '{print $1}')
+readonly ckpath="/Users/$username/.ck"
+
+# remove if folder exists
+installed=$(ls -al /Users/"$username"/ | grep .ck | wc -l | awk '{print $1}')
+if [ "$installed" = 1 ] ; then
+    rm -rf "$ckpath"
+fi
 
 # get source files
-git clone https://github.com/Allen-Hu/CompetitiveKotlin.git ./.ck
+git clone https://github.com/Allen-Hu/CompetitiveKotlin.git /Users/"$username"/.ck
 
 # add folder to path
-has_path=$(cat /etc/paths | grep /Users/"$username"/.ck | wc -l | awk '{ print($1) }')
+has_path=$(cat /etc/paths | grep "$ckpath" | wc -l | awk '{ print $1 }')
 if [ "$has_path" = 0 ] ; then
-    sh -c "echo /Users/$username/.ck >> /etc/paths"
+    sh -c "echo $ckpath >> /etc/paths"
 fi
 
 # give executing permissions
-chmod a+x /Users/"$username"/.ck/kk
-chmod a+x /Users/"$username"/.ck/kj
+chmod a+x "$ckpath"/kk
+chmod a+x "$ckpath"/kj
